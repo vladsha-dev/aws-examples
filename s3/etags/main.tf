fb700e7bcf2c9f98cf7254a5b60b2c66f9f68028
vladsha-dev <newshapovalov2002@gmail.com>
2026-04-08 10:20:56 +0200

    Add Terraform S3 object etag example.
    Include a minimal S3 bucket and object configuration with filemd5-based etag tracking and a lockfile for provider consistency.

---
+++ s3/etags/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.39.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_s3_bucket" "default" {
+}
+
+resource "aws_s3_object" "object" {
+  bucket = aws_s3_bucket.default.id
+  key    = "myfile.txt"
+  source = "myfile.txt"
+
+  etag = filemd5("myfile.txt")
+}
